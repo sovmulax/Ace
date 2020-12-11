a2cecc74085bf9c43a4a0e8a635a9fa070b73176
evane <soumailaevane@gmail.fr>
2020-12-11 16:22:12 +0000

    backend step 1 finie

--- php/connexion.php
+++ php/connexion.php
@@ -1,12 +1,12 @@
 <?php
 #section 1
-$connexion = new PDO("mysql:host=localhost;dbname=web", "root", "astrid");
+$connexion = new PDO("mysql:host=localhost;dbname=ace", "root", "astrid");
 $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
 
 <?php
   #section 2
-  $conn = mysqli_connect('localhost', 'root', 'astrid', 'web');
+  $conn = mysqli_connect('localhost', 'root', 'astrid', 'ace');
   // check connection
   if (!$conn) {
   echo 'Connection error: ' . mysqli_connect_error();
