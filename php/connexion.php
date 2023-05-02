47ae4df490f4b13de6aacdc198a93ecff0221eea
sovmulax <soumailaevane@gmail.com>
2023-05-02 12:08:49 +0000

    v

--- php/connexion.php
+++ php/connexion.php
@@ -1,12 +1,13 @@
 <?php
 #section 1
-$connexion = new PDO("mysql:host=localhost;dbname=ace", "root", "astrid");
+$connexion = new PDO("mysql:host=localhost;dbname=ace", "root", "latash");
 $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 ?>
 
 <?php
   #section 2
-  $conn = mysqli_connect('localhost', 'root', 'astrid', 'ace');
+  $conn = mysqli_connect('localhost', 'root', 'latash', 'ace');
   // check connection
   if (!$conn) {
   echo 'Connection error: ' . mysqli_connect_error();
